e3be1ca5937fbe43d90e301db82c0eef7054f5cd
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-06-01 16:47:22 +0200

    Grouplayer: use GroupFeature class for features

--- src/GroupFeature.js
+++ src/GroupFeature.js
@@ -1,12 +1,15 @@
 const GroupObject = require('./GroupObject')
+const SublayerFeature = require('./SublayerFeature')
 
-class GroupFeature {
+class GroupFeature extends SublayerFeature {
   constructor (id, sublayer) {
+    const object = new GroupObject(id)
+    super(object, sublayer)
+    this.object = object
     this.id = id
     this.sublayer = sublayer
     this.isShown = false
     this.flags = {}
-    this.object = new GroupObject(id)
   }
 }
 
