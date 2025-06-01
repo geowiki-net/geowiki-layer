4894d4ea6ffbd3f313cd7f46b7272f5bcf6d668c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-06-01 17:32:04 +0200

    GroupFeature: fix creating group features

--- src/GroupFeature.js
+++ src/GroupFeature.js
@@ -2,14 +2,8 @@ const GroupObject = require('./GroupObject')
 const SublayerFeature = require('./SublayerFeature')
 
 class GroupFeature extends SublayerFeature {
-  constructor (id, sublayer) {
-    const object = new GroupObject(id)
+  constructor (object, sublayer) {
     super(object, sublayer)
-    this.object = object
-    this.id = id
-    this.sublayer = sublayer
-    this.isShown = false
-    this.flags = {}
   }
 
   compileTwigData () {
