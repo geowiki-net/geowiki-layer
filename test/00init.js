20d71b9207b8bd63fac3cfb891526749136f4763
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-06-06 21:16:29 +0200

    Test: check list of items

--- test/00init.js
+++ test/00init.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+
 const GeowikiLayer = require('..')
 let geowikiAPI
 let geowikiLayer
@@ -22,6 +24,9 @@ describe('init', function () {
   })
 
   it('GeowikiLayer moveto', function (done) {
+    const expected = ['w314245157', 'w314245158', 'w314245160', 'w314245161', 'w314245164', 'w314245165', 'w314245167', 'w314245168', 'w314245169', 'w314245170', 'w314245171', 'w314245172', 'w314245176', 'r4222639', 'r4222640'].sort()
+    const found = []
+
     geowikiLayer.moveTo({
       bounds: {
         minlat: 48.198, minlon: 16.337,
@@ -29,7 +34,12 @@ describe('init', function () {
       },
       zoom: 18
     }, function () {
+      assert.deepEqual(found.sort(), expected, 'Wrong list of map items found')
       done()
+    })
+
+    geowikiLayer.on('add', (ob, feature) => {
+      found.push(feature.id)
     })
   })
 })
