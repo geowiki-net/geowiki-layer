b6051169e8cf1a61be12cbbe4157f799c8e254e7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-06-06 21:02:27 +0200

    Test: set up a simple layer and move to boundary

--- test/00init.js
+++ test/00init.js
@@ -1,4 +1,6 @@
+const GeowikiLayer = require('..')
 let geowikiAPI
+let geowikiLayer
 
 describe('init', function () {
   it('initialize GeowikiAPI, start loading data', function () {
@@ -8,5 +10,26 @@ describe('init', function () {
   it('load data into GeowikiAPI', function (done) {
     this.timeout(20000)
     geowikiAPI.on('load', () => done())
+  })
+
+  it('initialize GeowikiLayer', function (done) {
+    geowikiLayer = new GeowikiLayer({
+      geowikiAPI,
+      query: 'nwr[building]'
+    })
+
+    done()
+  })
+
+  it('GeowikiLayer moveto', function (done) {
+    geowikiLayer.moveTo({
+      bounds: {
+        minlat: 48.198, minlon: 16.337,
+        maxlat: 48.199, maxlon: 16.338
+      },
+      zoom: 18
+    }, function () {
+      done()
+    })
   })
 })
