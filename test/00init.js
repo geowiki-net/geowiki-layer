27b4ec7b7c1641fd584fab867b702c5509dc1e40
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-06-07 08:08:54 +0200

    Tests: install add/remove handlers only once

--- test/00init.js
+++ test/00init.js
@@ -6,6 +6,9 @@ let geowikiAPI
 let geowikiLayer
 
 describe('init', function () {
+  let foundAdd
+  let foundRemove
+
   it('initialize GeowikiAPI, start loading data', function () {
     geowikiAPI = require('./src/geowikiAPI')
   })
@@ -25,8 +28,10 @@ describe('init', function () {
   })
 
   it('GeowikiLayer moveto', function (done) {
-    const expected = ['w314245157', 'w314245158', 'w314245160', 'w314245161', 'w314245164', 'w314245165', 'w314245167', 'w314245168', 'w314245169', 'w314245170', 'w314245171', 'w314245172', 'w314245176', 'r4222639', 'r4222640'].sort()
-    const found = []
+    const expectedAdd = ['w314245157', 'w314245158', 'w314245160', 'w314245161', 'w314245164', 'w314245165', 'w314245167', 'w314245168', 'w314245169', 'w314245170', 'w314245171', 'w314245172', 'w314245176', 'r4222639', 'r4222640'].sort()
+    const expectedRemove = []
+    foundAdd = []
+    foundRemove = []
 
     geowikiLayer.moveTo({
       bounds: {
@@ -37,28 +42,25 @@ describe('init', function () {
       },
       zoom: 18
     }, function () {
-      assert.deepEqual(found.sort(), expected, 'Wrong list of map items found')
+      assert.deepEqual(foundAdd.sort(), expectedAdd, 'Wrong list of added map items found')
+      assert.deepEqual(foundRemove.sort(), expectedRemove, 'Wrong list of removed map items found')
       done()
     })
 
+    // install add/remove handlers
     geowikiLayer.on('add', (ob, feature) => {
-      found.push(feature.id)
+      foundAdd.push(feature.id)
+    })
+    geowikiLayer.on('remove', (ob, feature) => {
+      foundRemove.push(feature.id)
     })
   })
 
   it('GeowikiLayer moveto', function (done) {
     const expectedAdd = ['w313063260', 'w314245155', 'r4199627', 'r4199634', 'r4222638'].sort()
     const expectedRemove = ['w314245164', 'w314245165', 'r4222639', 'r4222640'].sort()
-    const foundAdd = []
-    const foundRemove = []
-
-    geowikiLayer.on('add', (ob, feature) => {
-      foundAdd.push(feature.id)
-    })
-    geowikiLayer.on('remove', (ob, feature) => {
-      console.log(feature.id)
-      foundRemove.push(feature.id)
-    })
+    foundAdd = []
+    foundRemove = []
 
     geowikiLayer.moveTo({
       bounds: {
